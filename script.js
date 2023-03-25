cc534463beed3310c9541024c2cc53a035e6a7e5
MatthewFo9one <122304324+MatthewFo9one@users.noreply.github.com>
2023-03-25 21:37:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let vkl = document.querySelector('.vkl')
+let bulb = document.querySelector('.bulb')
+
+vkl.addEventListener('click',function (e) {
+    console.log('hi');
+    bulb.classList.toggle()
+
+})
