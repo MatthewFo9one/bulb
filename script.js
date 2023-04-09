bb501b01393bcf100dfb57e575a7eb8b86a77369
Matthew <122304324+MatthewFo9one@users.noreply.github.com>
2023-04-09 21:45:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let vkl = document.querySelector('.vkl')
 let bulb = document.querySelector('.bulb')
+let container = document.querySelector('.container')
+let vkl_p_o = document.querySelector('.vkl_p_o')
+let vkl_p_f = document.querySelector('.vkl_p_f')
 
 vkl.addEventListener('click',function (e) {
     console.log('hi');
-    bulb.classList.toggle()
-
+    bulb.classList.toggle('on')
+    container.classList.toggle('w')
+    vkl.classList.toggle('light')
+    vkl_p_f.classList.toggle('no')
+    vkl_p_o.classList.toggle('no')
 })
